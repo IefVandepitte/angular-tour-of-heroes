2c04014dc368324f95f79d7609920695ce2c85e7
Ief Vandepitte <ief.vandepitte@telenet.be>
2018-12-06 09:45:47 +0100

    charts.js geimplementeerd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { ChartsComponent } from './charts/charts.component';
+
 
 //The colon(:) in the path indicates that : id is a placeholder for a specific hero id.
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },  
-  { path: 'detail/:id', component: HeroDetailComponent }
+  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'charts', component: ChartsComponent }
 ];
 
 @NgModule({
